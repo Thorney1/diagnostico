7df0f981a4cabe6084406d700adb4eaa7cf10e17
Daniel Sepulveda <daniel-thorney@hotmail.com>
2023-03-13 21:11:32 -0300

    Update Programa.py
    comentarios
    
    Co-Authored-By: Diego Molina <77606137+SirDaXll@users.noreply.github.com>

--- Programa.py
+++ Programa.py
@@ -1,7 +1,9 @@
-from os import system
+from os import system #Libreria para limpiar la consola
 
 class Chistes:
     def __init__(self):
+        #Generamos un arreglo de tipo lista de diccionarios para 
+        #separar las claves y los valores
         self.chistes = [
             {"titulo": "Campo laboral", "texto": "\n¿Cuántos programadores hacen falta para cambiar una bombilla? \n–Ninguno, porque es un problema de hardware.\n"},
             {"titulo": "Todo bien, creo...", "texto": "\nError 0094782: No se detecta ningún teclado pulse una tecla para continuar."},
@@ -10,6 +12,7 @@ class Chistes:
             {"titulo": "Contados", "texto": "\nSólo hay 10 tipos de personas en este bendito mundo, las que entienden binario y las que no."}
         ]
     
+    #Funcion para imprimir el titulo de los chiste y los chistes tambien
     def mostrar_chiste(self):
         print(self.chistes[0]["titulo"]) # Mostrar el titulo del primer chiste
         print(self.chistes[0]["texto"]) # Mostrar el texto del primer chiste
@@ -17,17 +20,19 @@ class Chistes:
     
 chistes = Chistes()
 
+#Ciclo para imprimir la pregunta hasta tener la respuesta esperada
 while True:
     respuesta = input("¿Quieres ver un chiste? Responde con 'si' o 'no': ")
     if respuesta.lower() == "si":
-        system("cls")
+        system("cls") #orden para limpieza de la terminal
+        #Ciclo para mostrar los chistes hasta que el user cierre el programa
         while True:
             chistes.mostrar_chiste()
             input("\nPresione ENTER para el siguiente chiste o cierra si ya te aburrió\n")
             system("cls")
     elif respuesta.lower() == "no":
         print("Bueno, adiós!")
-        exit()
+        exit() #no quiere nada asi que cerramos
         
     elif respuesta != "si" or "no":
         system("cls")
